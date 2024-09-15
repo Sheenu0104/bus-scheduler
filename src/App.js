82013cd49ba3dbab3d07973db49decd5d0d83a01
Sheenu0104 <shreya0141jain@gmail.com>
2024-09-15 23:53:07 +0530

    Your commit message

--- src/App.js
+++ src/App.js
@@ -1,39 +1,4 @@
-// // src/App.js
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Navbar from './components/Navbar';
-// import Footer from './components/Footer';
-// import HomePage from './pages/HomePage';
-// import AboutPage from './pages/AboutPage';
-// import ServicesPage from './pages/ServicesPage';
-// import ContactPage from './pages/ContactPage';
-// import LoginPage from './pages/LoginPage';
 
-// import './App.css';
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <div className="App">
-//         <Navbar />
-//         <main>
-//           <Routes>
-//             <Route path="/" element={<HomePage />} />
-//             <Route path="/about" element={<AboutPage />} />
-//             <Route path="/services" element={<ServicesPage />} />
-//             <Route path="/contact" element={<ContactPage />} />
-//             <Route path="/login" element={<LoginPage />} />
-//             {/* <Route path="/signup" element={<SignUpPage />} /> */}
-//           </Routes>
-//         </main>
-//         <Footer />
-//       </div>
-//     </Router>
-//   );
-// };
-
-// export default App;
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
@@ -43,7 +8,7 @@ import AboutPage from './pages/AboutPage';
 import ServicesPage from './pages/ServicesPage';
 import ContactPage from './pages/ContactPage';
 import LoginPage from './pages/LoginPage';
-import SignUpPage from './pages/SignUpPage'; // Ensure this import is correct
+
 import './App.css';
 
 const App = () => {
@@ -58,7 +23,7 @@ const App = () => {
             <Route path="/services" element={<ServicesPage />} />
             <Route path="/contact" element={<ContactPage />} />
             <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignUpPage />} /> {/* Ensure this line is correct */}
+            {/* <Route path="/signup" element={<SignUpPage />} /> Ensure this line is correct */}
           </Routes>
         </main>
         <Footer />
